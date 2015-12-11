669cc747f134907d32f4320c719480d6fd9175d5
Sergey Romanov <xxsmotur@gmail.com>
2015-12-11 20:36:40 +0500

    Add more API

--- ernyi/ernyi.go
+++ ernyi/ernyi.go
@@ -5,10 +5,12 @@ import (
     "sync"
     "errors"
     "log"
+    "fmt"
 )
 
 var (
 	errEmptyName = errors.New("Member must contain address")
+	errEmptyListMembers = errors.New("List of members is empty")
 )
 
 type Ernyi struct {
@@ -27,15 +29,51 @@ func CreateErnyi(config *Config)*Ernyi {
 	return ern
 }
 
-// Joib provides joining of the new member
+// Join provides joining of the new member
 func (ern *Ernyi) Join(addr string) error{
 	if addr == "" {
 		return errEmptyName
 	}
 
-	ern.mlist.Join([]string{addr})
+	nummembers, err := ern.mlist.Join([]string{addr})
+	if err != nil {
+		return err
+	}
+
+	if nummembers != 1 {
+		return fmt.Errorf("Expected number of joining nodes %d. Found - %d", 1, nummembers)
+	}
+
+	return nil
 }
 
+// JoinMany provides joining several nodes at once
+func (ern *Ernyi) JoinMany(addrs []string) error {
+	if len(addrs) == 0 {
+		return errEmptyListMembers
+	}
+	nummembers, err := ern.mlist.Join(addr)
+	if err != nil {
+		return err
+	}
+
+	if len(addrs) != nummembers {
+		return fmt.Errorf("Expected number of joining nodes %d. Found - %d", len(addrs), 
+			nummembers)
+	}
+	return nil
+}
+
+// Stop provides stopping of Ernyi
+func (ern *Ernyi) Stop() error {
+	var err error
+	 err = ern.mlist.Shutdown()
+	 if err != nil {
+	 	return err
+	 }
+}
+
+// Members return current alive members
 func (ern *Ernyi) Members()[]*memberlist.Node {
 	return ern.Members()
 }
