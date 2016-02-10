ff5af0c3d50e2cad68489158a7d3a0613dbdfdc4
Sergey Romanov <xxsmotur@gmail.com>
2016-02-10 23:51:01 +0500

    Add CrtTime parameter

--- ernyi/member.go
+++ ernyi/member.go
@@ -1,9 +1,15 @@
 package ernyi
 
+import (
+   "time"
+)
+
 // Member provides basic member for Ernyi
 type Member struct {
 	// Address of the node
 	Addr string
 	// Name of the node
 	Name string
+	// Time whn node is added
+	CrtTime time.Time
 }
